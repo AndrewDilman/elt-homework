6e9af43445056ef007fa846ba02cbaf0dd423b99
AndrewD <andreidilman2004@mail.ru>
2025-03-11 17:31:12 +0700

    working 4 task and polishing

--- h01/src/01.c
+++ h01/src/01.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    int a = 0; // каст нужен чтобы небыло ошибки
-
-    
-    printf("Enter positive or negative number: \n");
+int read_number(int* number){
     char buff[256];
     if (fgets(buff, sizeof(buff), stdin)) {
-        if (1 == sscanf(buff, "%d", &a)) { // каст нужен чтобы небыло ошибки
-            printf("%d\n", a);
+        if (sscanf(buff, "%d", number) == 0) {
+            printf("can't read number!\n");
+            return 0;
         }
     }
-    printf("Your number: %d\n", a);
-    printf("Binary representation:");
+    return 1;  
+}
 
+void replace_third_byte(int* number, int* replace){
+    int32_t n_buf = *number;
+    int32_t r_buf = *replace;
+
+    r_buf = r_buf << (4*2); // смещаем число на 3 байт
+    n_buf = n_buf & 0xFFFFF0FF; // маска убирающая 3 байт
+    n_buf = n_buf | r_buf; // Совмещаем два числа
+    *number = n_buf;
+}
+
+int print_and_count_num(int* number){
     int one_count = 0;
-    for(int i = 0; i<8 ; i++)
+    for(int i = 0; i<32 ; i++)
     {
-        int bit = !!((a<<i)  & 0x80);
+        int bit = !!((*number<<i) & 0x80000000);
         printf("%d", bit);
         /* двойное отрицание нужно, чтобы после применения маски 
         на вывод приходил просто 0 или 1*/
@@ -28,6 +35,34 @@ int main()
         (bit > 0) ? one_count++ : 0;
     }
     printf("\n");
-    printf("One count in binary: %d\n", one_count);
+    return one_count;
+}
 
+int main()
+{
+    int32_t number = 0;    
+    printf("Enter positive or negative number: \n");
+    if( !read_number(&number) )
+        return 0;
+    
+    // вывод числа и подсчёт единиц
+    printf("Binary representation:\n");
+    int one_count = print_and_count_num(&number);
+    
+    printf("One count in binary: %d\n", one_count);
+    
+    // замена третьего байта
+    int32_t replace = 0;
+    printf("Enter number to replace third byte: \n");
+    if( !read_number(&replace) )
+        return 0;
+    if(replace > 15){
+        printf("Maximum is 15\n");
+        replace = 15;
+    }
+    
+    replace_third_byte(&number, &replace);
+    print_and_count_num(&number);
+    
+    
 }
